9a1442f56d71e22a9bb9c7d2ec16e4d34968b859
nguyenducquyen <nguyenducquyen2k1@gmail.com>
2024-02-06 13:54:56 +0700

    update cv 05

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(
   cors({
     // origin: "*",
     origin: [
-      "http://localhost:3000",
+      "http://localhost:8080",
       "http://127.0.0.1:8080",
       "http://localhost:5173",
       "http://127.0.0.1:5173",
